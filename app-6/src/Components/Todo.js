ed2c13d40f83f784ea5209599f7468e3a5ef77ca
Jeordin Callister <jeordman@Jeordins-MacBook-Air.local>
2019-06-19 08:50:37 -0600

    DONE

--- app-6/src/Components/Todo.js
+++ app-6/src/Components/Todo.js
@@ -1,8 +1,11 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
 
 class Todo extends Component {
     render() {
-        return <p>{props.task}</p>
+        return(
+            <p>{this.props.task}</p>
+        )
     }
 }
 
